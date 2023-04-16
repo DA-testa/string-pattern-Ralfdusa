ce3973652fcafc1aaaea173fb1475c30eb4708dc
Ralfdusa <125834007+Ralfdusa@users.noreply.github.com>
2023-04-16 22:07:16 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,36 +1,45 @@
-def read_input():
-    ievade = input().strip()
-    if "I" in ievade:
+BASE = 101
+MOD = int(1e9 + 9)
+
+def get_hash(s):
+    h = 0
+    for c in s:
+        h = (h * BASE + ord(c)) % MOD
+    return h
+
+def get_occurrences(pattern, text):
+    n = len(text)
+    m = len(pattern)
+    p_hash = get_hash(pattern)
+    t_hash = get_hash(text[:m])
+    occurrences = []
+    for i in range(n - m + 1):
+        if t_hash == p_hash and text[i:i+m] == pattern:
+            occurrences.append(i)
+        if i < n - m:
+            # Update rolling hash by removing leftmost character and adding rightmost character
+            t_hash = ((t_hash - ord(text[i]) * pow(BASE, m-1, MOD)) * BASE + ord(text[i+m])) % MOD
+    return occurrences
+
+def main():
+    source = input().strip()
+    if source == 'I':
         pattern = input().strip()
         text = input().strip()
-    elif "F" in ievade:
-        cels = './tests/06'
-        try:
-            with open(cels, mode="r")as f:
-                pattern = f.readline().strip()
-                text = f.readline().strip()
-        except Exception as y:
-            print("kluda:", str(y))
-            return
+    elif source == 'F':
+        with open('tests/06') as f:
+            pattern = f.readline().strip()
+            text = f.readline().strip()
     else:
-        print("Error")
+        print('Invalid input source')
         return
-    return pattern, text
+    occurrences = get_occurrences(pattern, text)
+    print_occurrences(occurrences)
 
-def print_occurrences(output):
-    print(" ".join(map(str, output)))
-  
-def get_occurrences(pattern, text):
- lengthp = len(pattern)
- lengtht=len(text)
- hashp=hash(pattern)
- hasht=hash(text[:lengthp])
- occurrences = []
- for i in range(lengtht-lengthp + 1):
-    if hasht == hashp and pattern==text[i:i+lengthp]:
-        occurrences.append(i)
-    if(i<lengtht-lengthp):
-        hasht=hash(text[i+1: i+lengthp+1])
- return occurrences
+def print_occurrences(occurrences):
+    print(' '.join(map(str, occurrences)))
+
 if __name__ == '__main__':
-    print_occurrences(get_occurrences(*read_input()))
+    main()
+
+    
